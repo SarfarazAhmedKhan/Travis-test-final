12e5f9bc87b1e054e4c9cdf8ad4ddc2b23a883cd
sarfarazahmedkhankhan <sarfarazahmedkhankhan@gmail.com>
2020-06-27 17:58:15 +0500

    check now

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.status(200);
   res.json({
-    status: "Successfully connected with travis ci and elasticbeacnstalk!",
+    status: "Successfully changed here check it now again sir with travis ci and elasticbeacnstalk!",
   });
 });
 
 const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
-  console.log(`Successfully Test At ${PORT}`);
   console.log(`Successfully Test At ${PORT}`);
 });
